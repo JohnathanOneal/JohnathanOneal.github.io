fa9732fce011e898382a5b6ace1cde534dbdf49f
JohnathanOneal <johmathanoneal@gmail.com>
2024-11-08 17:58:38 -0500

    kenpom pull and svgs

---
+++ docs/scripts/kenpom_constants.py
@@ -0,0 +1,145 @@
+KENPOM_RATING = 'Rank'  # Team ranking based on overall performance
+TEAM = 'Team'  # Name of the team
+CONFERENCE = 'Conference'  # Name of the conference the team belongs to
+RECORD = 'Record'  # Team's win-loss record
+NET_RTG = 'NetRtg'  # Net rating, difference between offensive and defensive efficiency
+ADJUSTED_OFFENSIVE_EFFICIENCY = 'AdjOE'  # Adjusted offensive efficiency rating
+ADJUSTED_OFFENSIVE_EFFICIENCY_RANK = 'AdjOE_Rank'  # Rank of adjusted offensive efficiency
+ADJUSTED_DEFENSIVE_EFFICIENCY = 'AdjDE'  # Adjusted defensive efficiency rating
+ADJUSTED_DEFENSIVE_EFFICIENCY_RANK = 'AdjDE_Rank'  # Rank of adjusted defensive efficiency
+ADJUSTED_TEMPO = 'AdjT'  # Adjusted tempo, the speed at which a team plays
+ADJUSTED_TEMPO_RANK = 'AdjT_Rank'  # Rank of the adjusted tempo
+LUCK = 'Luck'  # Luck factor, how much a team's performance deviates from expected
+LUCK_RANK = 'Luck_Rank'  # Rank of the team's luck factor
+SOS_NET_RTG = 'SOS_NetRtg'  # Strength of Schedule net rating
+SOS_NET_RTG_RANK = 'SOS_NetRtg_Rank'  # Rank of Strength of Schedule net rating
+SOS_OFFENSIVE_EFFICIENCY = 'SOS_ORtg'  # Strength of Schedule offensive efficiency
+SOS_OFFENSIVE_EFFICIENCY_RANK = 'SOS_ORtg_Rank'  # Rank of Strength of Schedule offensive efficiency
+SOS_DEFENSIVE_EFFICIENCY = 'SOS_DRtg'  # Strength of Schedule defensive efficiency
+SOS_DEFENSIVE_EFFICIENCY_RANK = 'SOS_DRtg_Rank'  # Rank of Strength of Schedule defensive efficiency
+NCSOS_NET_RTG = 'NCSOS_NetRtg'  # Non-Conference Strength of Schedule net rating
+NCSOS_NET_RTG_RANK = 'NCSOS_NetRtg_Rank'  # Rank of Non-Conference Strength of Schedule net rating
+RAW_TEMPO = 'RawT'  # Raw tempo, the actual number of possessions per game
+RAW_TEMPO_RANK = 'RawT_Rank'  # Rank of the raw tempo
+POS_OFFENSIVE_EFFICIENCY = 'POS_ORtg'  # Positive offensive efficiency (player-based)
+POS_OFFENSIVE_EFFICIENCY_RANK = 'POS_ORtg_Rank'  # Rank of positive offensive efficiency
+POS_DEFENSIVE_EFFICIENCY = 'POS_DRtg'  # Positive defensive efficiency (player-based)
+POS_DEFENSIVE_EFFICIENCY_RANK = 'POS_DRtg_Rank'  # Rank of positive defensive efficiency
+RAW_OFFENSIVE_EFFICIENCY = 'RawO'  # Raw offensive efficiency
+RAW_OFFENSIVE_EFFICIENCY_RANK = 'RawO_Rank'  # Rank of raw offensive efficiency
+RAW_DEFENSIVE_EFFICIENCY = 'RawD'  # Raw defensive efficiency
+RAW_DEFENSIVE_EFFICIENCY_RANK = 'RawD_Rank'  # Rank of raw defensive efficiency
+OEFG = 'OEFG'  # Offensive effective field goal percentage
+OEFG_RANK = 'OEFG_Rank'  # Rank of offensive effective field goal percentage
+OTO = 'OTO'  # Offensive turnover percentage
+OTO_RANK = 'OTO_Rank'  # Rank of offensive turnover percentage
+OOR = 'OOR'  # Offensive rebound percentage
+OOR_RANK = 'OOR_Rank'  # Rank of offensive rebound percentage
+OFTRATE = 'OFTRate'  # Offensive free throw rate
+OFTRATE_RANK = 'OFTRate_Rank'  # Rank of offensive free throw rate
+DEFG = 'DEFG'  # Defensive effective field goal percentage
+DEFG_RANK = 'DEFG_Rank'  # Rank of defensive effective field goal percentage
+DTO = 'DTO'  # Defensive turnover percentage
+DTO_RANK = 'DTO_Rank'  # Rank of defensive turnover percentage
+DOR = 'DOR'  # Defensive rebound percentage
+DOR_RANK = 'DOR_Rank'  # Rank of defensive rebound percentage
+DFTRATE = 'DFTRate'  # Defensive free throw rate
+DFTRATE_RANK = 'DFTRate_Rank'  # Rank of defensive free throw rate
+OFT = 'OFT'  # Offensive free throws
+OFT_RANK = 'OFT_Rank'  # Rank of offensive free throws
+OTWOP = 'OTWOP'  # Offensive two-point shots
+OTWOP_RANK = 'OTWOP_Rank'  # Rank of offensive two-point shots
+OTHREEP = 'OTHREEP'  # Offensive three-point shots
+OTHREEP_RANK = 'OTHREEP_Rank'  # Rank of offensive three-point shots
+DFT = 'DFT'  # Defensive free throws
+DFT_RANK = 'DFT_Rank'  # Rank of defensive free throws
+DTWOP = 'DTWOP'  # Defensive two-point shots
+DTWOP_RANK = 'DTWOP_Rank'  # Rank of defensive two-point shots
+DTHREEP = 'DTHREEP'  # Defensive three-point shots
+DTHREEP_RANK = 'DTHREEP_Rank'  # Rank of defensive three-point shots
+AVG_HEIGHT = 'Avg_Hgt'  # Average height of players on the team
+AVG_HEIGHT_RANK = 'Avg_Hgt_Rank'  # Rank of average height
+EFF_HEIGHT = 'Eff_Hgt'  # Effective height
+EFF_HEIGHT_RANK = 'Eff_Hgt_Rank'  # Rank of effective height
+C_HEIGHT = 'C_Hgt'  # Height of center players
+C_HEIGHT_RANK = 'C_Hgt_Rank'  # Rank of center height
+PF_HEIGHT = 'PF_Hgt'  # Height of power forward players
+PF_HEIGHT_RANK = 'PF_Hgt_Rank'  # Rank of power forward height
+SF_HEIGHT = 'SF_Hgt'  # Height of small forward players
+SF_HEIGHT_RANK = 'SF_Hgt_Rank'  # Rank of small forward height
+SG_HEIGHT = 'SG_Hgt'  # Height of shooting guard players
+SG_HEIGHT_RANK = 'SG_Hgt_Rank'  # Rank of shooting guard height
+PG_HEIGHT = 'PG_Hgt'  # Height of point guard players
+PG_HEIGHT_RANK = 'PG_Hgt_Rank'  # Rank of point guard height
+EXPERIENCE = 'Experience'  # Player experience
+EXPERIENCE_RANK = 'Experience_Rank'  # Rank of player experience
+BENCH = 'Bench'  # Bench depth
+BENCH_RANK = 'Bench_Rank'  # Rank of bench depth
+CONTINUITY = 'Continuity'  # Continuity of the team
+CONTINUITY_RANK = 'Continuity_Rank'  # Rank of team continuity
+THREE_POINT_PERCENTAGE = '3P_Pct'  # 3-point shooting percentage
+THREE_POINT_PERCENTAGE_RANK = '3P_Pct_Rank'  # Rank of 3-point shooting percentage
+TWO_POINT_PERCENTAGE = '2P_Pct'  # 2-point shooting percentage
+TWO_POINT_PERCENTAGE_RANK = '2P_Pct_Rank'  # Rank of 2-point shooting percentage
+FREE_THROW_PERCENTAGE = 'FT_Pct'  # Free throw shooting percentage
+FREE_THROW_PERCENTAGE_RANK = 'FT_Pct_Rank'  # Rank of free throw shooting percentage
+BLOCK_PERCENTAGE = 'Blk_Pct'  # Block percentage
+BLOCK_PERCENTAGE_RANK = 'Blk_Pct_Rank'  # Rank of block percentage
+STEAL_PERCENTAGE = 'Stl_Pct'  # Steal percentage
+STEAL_PERCENTAGE_RANK = 'Stl_Pct_Rank'  # Rank of steal percentage
+TURNOVER_PERCENTAGE = 'NST_Pct'  # Turnover percentage
+TURNOVER_PERCENTAGE_RANK = 'NST_Pct_Rank'  # Rank of turnover percentage
+ASSIST_PERCENTAGE = 'A_Pct'  # Assist percentage
+ASSIST_PERCENTAGE_RANK = 'A_Pct_Rank'  # Rank of assist percentage
+THREE_POINT_ATTEMPT_PERCENTAGE = '3PA_Pct'  # Percentage of 3-point attempts
+THREE_POINT_ATTEMPT_PERCENTAGE_RANK = '3PA_Pct_Rank'  # Rank of 3-point attempt percentage
+
+INDEX_COLUMN_HEADERS = [
+    KENPOM_RATING, TEAM, CONFERENCE, RECORD, NET_RTG, ADJUSTED_OFFENSIVE_EFFICIENCY, ADJUSTED_OFFENSIVE_EFFICIENCY_RANK,
+    ADJUSTED_DEFENSIVE_EFFICIENCY, ADJUSTED_DEFENSIVE_EFFICIENCY_RANK, ADJUSTED_TEMPO, ADJUSTED_TEMPO_RANK,
+    LUCK, LUCK_RANK, SOS_NET_RTG, SOS_NET_RTG_RANK, SOS_OFFENSIVE_EFFICIENCY, SOS_OFFENSIVE_EFFICIENCY_RANK,
+    SOS_DEFENSIVE_EFFICIENCY, SOS_DEFENSIVE_EFFICIENCY_RANK, NCSOS_NET_RTG, NCSOS_NET_RTG_RANK
+]
+
+SUMMARY_COLUMN_HEADERS = [
+    TEAM, CONFERENCE, ADJUSTED_TEMPO, ADJUSTED_TEMPO_RANK, RAW_TEMPO, RAW_TEMPO_RANK,
+    POS_OFFENSIVE_EFFICIENCY, POS_OFFENSIVE_EFFICIENCY_RANK, POS_DEFENSIVE_EFFICIENCY,
+    POS_DEFENSIVE_EFFICIENCY_RANK, ADJUSTED_OFFENSIVE_EFFICIENCY, ADJUSTED_OFFENSIVE_EFFICIENCY_RANK,
+    RAW_OFFENSIVE_EFFICIENCY, RAW_OFFENSIVE_EFFICIENCY_RANK, ADJUSTED_DEFENSIVE_EFFICIENCY,
+    ADJUSTED_DEFENSIVE_EFFICIENCY_RANK, RAW_DEFENSIVE_EFFICIENCY, RAW_DEFENSIVE_EFFICIENCY_RANK
+]
+
+STATS_COLUMN_HEADERS = [
+    TEAM, CONFERENCE, ADJUSTED_TEMPO, ADJUSTED_TEMPO_RANK, ADJUSTED_OFFENSIVE_EFFICIENCY,
+    ADJUSTED_OFFENSIVE_EFFICIENCY_RANK, OEFG, OEFG_RANK, OTO, OTO_RANK, OOR, OOR_RANK, OFTRATE,
+    OFTRATE_RANK, ADJUSTED_DEFENSIVE_EFFICIENCY, ADJUSTED_DEFENSIVE_EFFICIENCY_RANK, DEFG, DEFG_RANK,
+    DTO, DTO_RANK, DOR, DOR_RANK, DFTRATE, DFTRATE_RANK
+]
+
+POINTDIST_COLUMN_HEADERS = [
+    TEAM, CONFERENCE, OFT, OFT_RANK, OTWOP, OTWOP_RANK, OTHREEP, OTHREEP_RANK, DFT, DFT_RANK,
+    DTWOP, DTWOP_RANK, DTHREEP, DTHREEP_RANK
+]
+
+HEIGHT_COLUMN_HEADERS = [
+    TEAM, CONFERENCE, AVG_HEIGHT, AVG_HEIGHT_RANK, EFF_HEIGHT, EFF_HEIGHT_RANK, C_HEIGHT,
+    C_HEIGHT_RANK, PF_HEIGHT, PF_HEIGHT_RANK, SF_HEIGHT, SF_HEIGHT_RANK, SG_HEIGHT, SG_HEIGHT_RANK,
+    PG_HEIGHT, PG_HEIGHT_RANK, EXPERIENCE, EXPERIENCE_RANK, BENCH, BENCH_RANK, CONTINUITY, CONTINUITY_RANK
+]
+
+TEAMSTATS_COLUMN_HEADERS = [
+    TEAM, CONFERENCE, THREE_POINT_PERCENTAGE, THREE_POINT_PERCENTAGE_RANK, TWO_POINT_PERCENTAGE,
+    TWO_POINT_PERCENTAGE_RANK, FREE_THROW_PERCENTAGE, FREE_THROW_PERCENTAGE_RANK, BLOCK_PERCENTAGE,
+    BLOCK_PERCENTAGE_RANK, STEAL_PERCENTAGE, STEAL_PERCENTAGE_RANK, TURNOVER_PERCENTAGE, TURNOVER_PERCENTAGE_RANK,
+    ASSIST_PERCENTAGE, ASSIST_PERCENTAGE_RANK, THREE_POINT_ATTEMPT_PERCENTAGE, THREE_POINT_ATTEMPT_PERCENTAGE_RANK,
+    ADJUSTED_OFFENSIVE_EFFICIENCY, ADJUSTED_OFFENSIVE_EFFICIENCY_RANK
+]
+
+PAGE_INFO = {'https://kenpom.com/index.php': INDEX_COLUMN_HEADERS,
+             'https://kenpom.com/summary.php': SUMMARY_COLUMN_HEADERS,
+             'https://kenpom.com/stats.php': STATS_COLUMN_HEADERS,
+             'https://kenpom.com/pointdist.php': POINTDIST_COLUMN_HEADERS,
+             'https://kenpom.com/height.php': HEIGHT_COLUMN_HEADERS,
+             'https://kenpom.com/teamstats.php': TEAMSTATS_COLUMN_HEADERS
+             }
+
---
+++ docs/scripts/kenpom_pull.py
@@ -0,0 +1,145 @@
+import pandas as pd
+from playwright.sync_api import sync_playwright
+import kenpom_constants
+import boto3
+import os
+from botocore.exceptions import ClientError
+
+
+def extract_table_rows(page):
+    """Extract the table rows from the page."""
+    return page.evaluate("""
+        () => {
+            return Array.from(document.querySelectorAll('#ratings-table tbody tr'))
+                        .map(row => Array.from(row.querySelectorAll('td'))
+                             .map(td => td.innerText.trim()));
+        }
+    """)
+
+
+def is_current_season(season):
+    now = pd.Timestamp.now()
+    return season == now.year if now.month <= 5 else season - 1 == now.year
+
+
+def scrape_kenpom_table(page, url, year):
+
+    """Scrape the KenPom table from the page."""
+    headers = kenpom_constants.PAGE_INFO[url]
+    rows = extract_table_rows(page)
+
+    # Check if the number of rows matches the number of columns
+    if len(rows) > 0 and len(rows[0]) != len(headers):
+        raise ValueError(f"Number of columns ({len(headers)}) does not match number of columns in data ({len(rows[0])})")
+
+    # Create a DataFrame from the extracted data
+    df = pd.DataFrame(rows, columns=headers)
+
+    # Convert columns to appropriate data types
+    if 'Kenpom_Rating' in df.columns:
+        df['Kenpom_Rating'] = df['Kenpom_Rating'].astype(int)
+    if 'Record' in df.columns:
+        df[['Wins', 'Losses']] = df['Record'].str.split('-', expand=True)
+        df = df.drop(columns='Record')
+
+    for col in df.columns:
+        if col.endswith('Rtg') or col.endswith('T'):
+            df[col] = df[col].str.replace(',', '').astype(float)
+        elif col.endswith('_Rank'):
+            df[col] = df[col].str.extract(r'(\d+)', expand=False).astype(int)
+
+    df['Team'] = df['Team'].str.replace(r'[\d*]', '', regex=True).str.strip().str.lower()
+
+    df['Season'] = int(year)
+
+    return df
+
+
+def fetch_kenpom_data(begin_season=2025,end_season=2025):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        # Login to kenpom
+        page.goto("https://kenpom.com/")
+        page.fill("input[name='email']", os.environ['KENPOM_USERNAME'])
+        page.fill("input[name='password']", os.environ['KENPOM_PASSWORD'])
+        page.click("input[type='submit']")
+
+        # Wait for navigation to complete
+        page.wait_for_timeout(7000)
+
+        seasonFrames = {}
+
+        # Confirm login success and scrape data
+        if "logout" in page.content():
+
+            for season in range(begin_season, end_season + 1):
+                frames = []
+                page.wait_for_timeout(4000)
+                for url in kenpom_constants.PAGE_INFO.keys():
+                    page.wait_for_timeout(5000)
+                    page.goto(f"{url}?y={season}")
+                    df = scrape_kenpom_table(page, url, season)
+                    frames.append(df)
+
+                merged_df = frames[0]
+                for df in frames[1:]:
+                    merged_df = pd.merge(merged_df, df, on=['Team', 'Season'], how='outer', suffixes=('', '_drop'))
+                merged_df = merged_df.loc[:, ~merged_df.columns.str.endswith('_drop')]
+
+                if is_current_season(season):
+                    month_and_day = pd.Timestamp.now().strftime('%m%d')
+                    seasonFrames.update({f"{season}_{month_and_day}": merged_df})
+                else:
+                    seasonFrames.update({f"{season}": merged_df})
+
+        else:
+            print("Login failed")
+
+        browser.close()
+        return seasonFrames
+
+def upload_to_s3(s3_client, bucket_name, filename, data):
+    try:
+        s3_client.put_object(
+            Bucket=bucket_name,
+            Key=filename,
+            Body=data,
+            ContentType='text/csv'
+        )
+        print(f"Successfully uploaded {filename} to S3 bucket {bucket_name}")
+    except ClientError as e:
+        raise Exception(f"Error uploading {filename} to S3: {e}")
+
+def main(begin_season, end_season):
+    # Initialize S3 client and bucket name
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+    )
+    bucket_name = os.environ['S3_BUCKET_NAME']
+
+    # Fetch data for each season
+    seasonFrames = fetch_kenpom_data(begin_season=begin_season, end_season=end_season)
+    
+    # Loop through each season's DataFrame and upload it to S3
+    for key, value in seasonFrames.items():
+        # Convert DataFrame to CSV format (in memory) for uploading
+        csv_data = value.to_csv(index=False)
+        filename = f"kenpom_{key}.csv"
+        
+        # Upload CSV data to S3
+        upload_to_s3(s3, bucket_name, filename, csv_data)
+
+if __name__ == "__main__":
+    # Argument parser for command-line arguments
+    parser = argparse.ArgumentParser(description="Upload KenPom data to S3")
+    parser.add_argument("--b", "--begin_season", type=int, required=True, help="Beginning season year")
+    parser.add_argument("--e", "--end_season", type=int, required=True, help="Ending season year")
+    args = parser.parse_args()
+    
+    # Run the main function with provided begin and end season years
+    main(begin_season=args.b, end_season=args.e)
+
